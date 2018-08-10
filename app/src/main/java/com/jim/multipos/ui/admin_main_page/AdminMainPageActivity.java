40dfd94e57edcb6400bfa6e90e2718ab013902d6
sardor.karimov@gmail.com <0504694814s>
2018-08-10 12:43:41 +0500

    admin page

--- app/src/main/java/com/jim/multipos/ui/admin_main_page/AdminMainPageActivity.java
+++ app/src/main/java/com/jim/multipos/ui/admin_main_page/AdminMainPageActivity.java
@@ -3,6 +3,7 @@ package com.jim.multipos.ui.admin_main_page;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.jim.mpviews.MpSpinnerTransparent;
 import com.jim.mpviews.MpToolbar;
 import com.jim.multipos.R;
 import com.jim.multipos.core.DoubleSideAdminActivity;
@@ -13,5 +14,8 @@ public class AdminMainPageActivity extends DoubleSideAdminActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((MpToolbar) findViewById(R.id.toolbar)).setMode(MpToolbar.ADMIN_MODE);
+        ((MpSpinnerTransparent) findViewById(R.id.trans_spinner)).setItems(new String[]{"John John", "Shean Shean"}, new String[]{"1", "2"}, new String[]{"123"});
+        ((MpSpinnerTransparent) findViewById(R.id.trans_spinner)).setAdapter();
+
     }
 }
